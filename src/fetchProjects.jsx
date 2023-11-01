cfe415f3223e9633a481d5a2d84242e2f89a27d7
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-11-01 03:05:46 +0000

    read me updated

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,48 +1,41 @@
-
-import { useState, useEffect } from 'react'
-import {createClient} from 'contentful'
-
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
 
 const client = createClient({
-    space:'34tz63xsp3gq',
-    environment: 'master',
-    accessToken: import.meta.env.VITE_API_KEY,
+  space: "34tz63xsp3gq",
+  environment: "master",
+  accessToken: import.meta.env.VITE_API_KEY,
 });
- // import.meta.env.VITE_API_KEY,
+// import.meta.env.VITE_API_KEY,
 // rm -rf .git
 export const useFetchProjects = () => {
-    const [loading, setLoading] = useState(true)
-    const [projects, setProjects] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
 
-console.log(projects);
-const getData = async()=> {
-
+  const getData = async () => {
     try {
-            const response = await client.getEntries({content_type: 'projects'});
-            const projects = response.items.map((item) => {
-            const {title, url, image} = item.fields;
-            const id = item.sys.id;
-                const img = image?.fields?.file?.url;
-                return {title, url, id, img};
-            })
-            setProjects(projects)
-            setLoading(false)
- 
-        } catch (error) {
-            console.log(error);
-            setLoading(false)
-        }
+      const response = await client.getEntries({ content_type: "projects" });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
-    useEffect(()=>{
-        getData()
-        console.log('hello');
-     },[]) 
+  };
+  useEffect(() => {
+    getData();
+    console.log("hello");
+  }, []);
 
-
-     return {loading, projects}
-    }
+  return { loading, projects };
+};
 
 // client.getEntries({content_type:"projects"})
 // .then((response) => console.log(response))
-
- 
