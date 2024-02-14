f1fafb9aa3853b4530cb53597ea047a54dbeadf0
MAC-ANTHONY <macanthonygreat@gmail.com>
2024-02-14 15:06:26 +0000

    hero message

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import heroImg from './assets/hero.svg'
+import React from "react";
+import heroImg from "./assets/hero.svg";
 
-const Hero = ()=> {
-return (<section className="hero">
-<div className="hero-center">
-    <div className="hero-title">
-        <h1>Contentful CMS</h1>
-    <p>
-        ipsum, dolor sit amet consectetur 
-         adipisicing elit. Voluptatibus a, optio
-        impedit tempore necessitatibus corrupti
-        officia sed cumque consequuntur qui ne
-        que architecto saepe repellat repellend
-        us dolor exercitationem esse ut quos.
-        Lorem ipsum, dolor sit amet consectetur 
-         adipisicing elit. Voluptatibus a, optio
-        impedit tempore necessitatibus corrupti
-        officia sed cumque consequuntur qui ne
-        que architecto saepe repellat repellend
-        us dolor exercitationem esse ut quos.
-    </p>
-    </div>
-    <div className="img-container">
-        <img src={heroImg} alt="woman and the browser" className='img' />
-    </div>
-</div>
-</section>
-)}
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            A headless CMS is a back-end only content management system that
+            provides content creators with an intuitive interface for creating
+            and managing content, while leaving the front-end presentation layer
+            to be handled by a separate system or platform. This approach allows
+            for greater flexibility and scalability, as the content can be
+            easily distributed to multiple channels and devices, without being
+            limited by the constraints of a particular front-end system.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Hero;
